feeaa57c8291c372e53caac98e312fb6d8aa0f77
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-04-10 18:35:30 -0600

    Add files via upload

---
+++ E1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int an;
+	int seg;
+
+	printf("Cuantos aros tienes:\n");
+	scanf("%d", &an);
+	seg = an * 365 * 24 * 60 * 60;
+	
+printf("La cantidad de segundos que has vivido es: %d\n", seg);
+	
+	return 0;
+}
+
