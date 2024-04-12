9781a7f564c66a1038eca25ed21702460860c9ec
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-04-11 20:39:26 -0600

    Add files via upload

---
+++ tar.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    printf("  |");
+    for (i = 1; i <= 10; ++i) {
+        printf("%4d", i);
+    }
+    printf("\n--+----------------------------------------\n");
+
+    for (i = 1; i <= 10; ++i) {
+        printf("%2d|", i);
+        for (j = 1; j <= 10; ++j) {
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
